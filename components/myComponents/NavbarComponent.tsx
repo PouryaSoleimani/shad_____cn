8a1cfb2d41cb680a631eb389d72ec5ef5a098a24
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-04-12 20:28:56 +0330

    ⬛ SHAD______CN ⬛

--- components/myComponents/NavbarComponent.tsx
+++ components/myComponents/NavbarComponent.tsx
@@ -36,9 +36,7 @@ export function NavbarComponent() {
                                     <NavigationMenuLink asChild>
                                         <a className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md" href="/"  >
                                             <SiAdidas />
-                                            <div className="mb-2 mt-4 text-lg font-medium">
-                                                shadcn/ui
-                                            </div>
+                                            <div className="mb-2 mt-4 text-lg font-medium">  shadcn/ui </div>
                                             <p className="text-sm leading-tight text-muted-foreground">      Beautifully designed components built with Radix UI and      Tailwind CSS.  </p>
                                         </a>
                                     </NavigationMenuLink>

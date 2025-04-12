88291d51f18041b25e31b11fab150afed699a4db
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-04-12 22:56:06 +0330

    ⬛ SHAD______CN ⬛

--- components/myComponents/FormComponent.tsx
+++ components/myComponents/FormComponent.tsx
@@ -1,61 +1,44 @@
-"use client"
-import { password } from 'bun'
 import React from 'react'
-import { z } from "zod"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import { Button } from "@/components/ui/button"
-import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage, } from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
+import { useForm, SubmitHandler } from "react-hook-form"
+import { Button } from '../ui/button'
+import { yupResolver } from "@hookform/resolvers/yup"
+import * as yup from "yup"
+import { Toaster, toast } from 'sonner'
+
+type Inputs = { username: string, password: string }
+const schema = yup.object({ username: yup.string().required(), password: yup.string().required(), }).required()
 
 
-//  FORM SCHEMA
-const formSchema = z.object({
-    username: z.string().min(2).max(50),
-    password: z.string().min(8, { message: "Password must be at least 8 characters." }).max(20),
-})
-
-//^ COMPONENT :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+// COMPONENT =================================================================================================================================================================
 function FormComponent() {
-    // USE FORM
-    const form = useForm<z.infer<typeof formSchema>>({
-        resolver: zodResolver(formSchema),
-        defaultValues: { username: "", password: "" },
-    })
 
-    // ON SUBMIT
-    function onSubmit(values: z.infer<typeof formSchema>) {
-        console.info(values)
+    const { register, handleSubmit, formState: { errors }, reset } = useForm<Inputs>({ resolver: yupResolver(schema), })
+
+    const onSubmit: SubmitHandler<Inputs> = (data) => {
+        console.info(data);
+        reset();
+        toast.message('Welcome Back !', {
+            description: 'Monday, January 3rd at 6:00pm',
+        })
     }
 
-
     return (
-        <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 w-1/5 p-4 border mx-auto my-16 rounded-lg bg-black/10 backdrop-blur-xl h-64 ">
-                <FormField control={form.control} name="username" render={({ field }) => (
-                    <FormItem>
-                        <FormLabel>Username</FormLabel>
-                        <FormControl>
-                            <Input placeholder="Username" {...field} className='bg-zinc-900 text-white px-2' />
-                        </FormControl>
-                        <FormMessage />
-                        <FormLabel>Password</FormLabel>
-                        <FormControl>
-                            <Input type="password" placeholder="Password" {...field} className='bg-zinc-900 text-white px-2' />
-                        </FormControl>
-                        <FormDescription>
-                            This is your public display name.
-                        </FormDescription>
-                        <FormMessage />
-                    </FormItem>
-                )}
-                />
-                <Button type="submit" className='w-full bg-emerald-800 hover:bg-emerald-950 duration-300 rounded font-bold text-white'>Submit</Button>
-            </form>
-        </Form>
+        <>
+            <Toaster position="top-right" expand={true} />
+            <div>
+                <form onSubmit={handleSubmit(onSubmit)} className='w-fit border flex flex-col items-stretch justify-start p-6 rounded mx-auto my-16 gap-y-1 shadow-lg shadow-white/10 h-[22rem]'>
+                    <h2 className='w-full text-center bg-blue-950 py-5 rounded font-black'>SIGN IN</h2>
+                    <label htmlFor="username">Username</label>
+                    <input id='username' className='bg-zinc-900 px-4 py-2 rounded w-64 outline-0 text-white font-semibold' {...register("username")} />
+                    {errors.username && <span className='text-red-900 text-sm font-semibold pl-1 m-0 p-0 tracking-tighter'>Username is required</span>}
+                    <label htmlFor="password">Password</label>
+                    <input type='password' className='bg-zinc-900 px-4 py-2 rounded w-64 outline-0 text-white font-semibold' {...register("password")} />
+                    {errors.password && <span className='text-red-900 text-sm font-semibold pl-1 m-0 p-0 tracking-tighter'>Password is required</span>}
+                    <Button className='bg-green-950 hover:bg-green-800 py-6 text-white font-bold rounded border-none mt-10'>Submit</Button>
+                </form>
+            </div>
+        </>
     )
-
-
 }
 
 export default FormComponent
